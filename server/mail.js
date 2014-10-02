17a6318e171b68a3981f6963318e7697bd6e107b
Jeremiah Goyette <jsgoyette@gmail.com>
2014-10-01 21:36:11 -0500

    changed startup name to mail

---
+++ server/mail.js
@@ -0,0 +1,3 @@
+Meteor.startup(function () {
+  process.env.MAIL_URL="smtp://example%40gmail.com:abcxyz@smtp.gmail.com:465";
+});
--- server/startup.js
+++
@@ -1,3 +0,0 @@
-Meteor.startup(function () {
-  process.env.MAIL_URL="smtp://jsgoyette%40gmail.com:pfrkkaolrqmznsnd@smtp.gmail.com:465";
-});
