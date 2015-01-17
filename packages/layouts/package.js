2de6d0d8d5c9208f00c43ab2ee8d8f18c4b45809
Jeremiah Goyette <jsgoyette@gmail.com>
2015-01-16 20:56:10 -0600

    added list view to layouts

--- packages/layouts/package.js
+++ packages/layouts/package.js
@@ -8,10 +8,17 @@ Package.onUse(function (api) {
 
   var both = ['client', 'server'];
 
+  api.use('tracker', both);
   api.use('mediator', both);
   api.use('underscore', both);
   api.use('templating', 'client');
+  api.use('aldeed:autoform', both);
+  api.use('aldeed:collection2', both);
+  api.use('aldeed:simple-schema', both);
   api.use('spin', 'client');
+
+  api.addFiles('layouts/list.html', 'client');
+  api.addFiles('layouts/list.js', 'client');
 
   api.addFiles('layouts/spinner.html', 'client');
   api.addFiles('layouts/spinner.js', 'client');
