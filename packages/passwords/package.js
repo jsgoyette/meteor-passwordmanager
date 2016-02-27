14cf634d7dca0fd3e5f7392326cd7d4a0666d969
Jeremiah Goyette <jsgoyette@gmail.com>
2016-02-27 17:59:37 -0600

    clean up passwords package definitions

--- packages/passwords/package.js
+++ packages/passwords/package.js
@@ -6,27 +6,36 @@ Package.describe({
 
 Package.onUse(function (api) {
 
-  api.use('underscore');
-  api.use('iron:router');
-  api.use('aldeed:autoform');
-  api.use('aldeed:collection2');
-  api.use('aldeed:simple-schema');
-  api.use('jsgoyette:mediator');
+  api.use([
+    'underscore',
+    'iron:router',
+    'aldeed:autoform',
+    'aldeed:collection2',
+    'aldeed:simple-schema',
+    'jsgoyette:mediator',
+  ]);
 
-  api.use('templating', 'client');
-  api.use('reactive-var', 'client');
-  api.use('sha', 'client');
-  api.use('jsgoyette:aes', 'client');
+  api.use([
+    'templating',
+    'reactive-var',
+    'jsgoyette:aes',
+    'sha',
+  ], 'client');
 
-  api.addFiles('definitions.js');
-  api.addFiles('collections.js');
-  api.addFiles('routes.js');
+  api.addFiles([
+    'definitions.js',
+    'collections.js',
+    'routes.js',
+  ]);
+
   api.addFiles('publications.js', 'server');
 
-  api.addFiles('views/password.html', 'client');
-  api.addFiles('views/password.js', 'client');
-  api.addFiles('views/passwordlist.html', 'client');
-  api.addFiles('views/passwordlist.js', 'client');
+  api.addFiles([
+    'views/password.html',
+    'views/password.js',
+    'views/passwordlist.html',
+    'views/passwordlist.js',
+  ], 'client');
 
   api.export('Passwords');
 });
