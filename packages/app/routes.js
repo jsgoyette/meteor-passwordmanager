a2127c3eb01ef5a7079bd96317953a7e3ec87d6c
Jeremiah Goyette <jsgoyette@gmail.com>
2016-03-02 19:19:31 -0600

    make secure message the home route '/'

--- packages/app/routes.js
+++ packages/app/routes.js
@@ -4,8 +4,11 @@ Router.configure({
 });
 
 Router.route('/', function() {
-  this.redirect('passwordlist');
+  this.redirect('secure-create');
+}, {
+  name: 'home'
 });
 
 Router.openRoutes.push('secure-create');
 Router.openRoutes.push('secure-retrieve');
+Router.openRoutes.push('home');
